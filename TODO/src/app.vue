e7682c5beac055aa38af559af47aa164f35740c0
buaadpy <502687001@qq.com>
2019-06-25 12:01:42 +0800

    Todo样式

---
+++ TODO/src/app.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div id="cover"></div>
+    <MyHeader></MyHeader>
+    <Todo></Todo>
+    <MyFooter></MyFooter>
+  </div>
+</template>
+
+<script>
+import MyHeader from "./todo/header.vue"
+import MyFooter from "./todo/footer.jsx"
+import Todo from "./todo/todo.vue"
+
+export default {
+  components: {
+    MyHeader, MyFooter,Todo
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+#app {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+
+#cover {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #999;
+  opacity: 0.9;
+  z-index: -1;
+}
+</style>
---
+++ TODO/src/todo/footer.jsx
@@ -0,0 +1,11 @@
+import '../assets/styles/footer.styl'
+
+export default {
+    render() {
+        return (
+            <div id="footer">
+                <span>Written by dpy</span>
+            </div>
+        )
+    }
+}
