fd035fedd958eadbfc906c48f6c7304e83f1b4b5
buaadpy <502687001@qq.com>
2019-07-04 20:03:00 +0800

    mytodo-production

---
+++ MyTodo/webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: ExtractPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            }),
+        }, {
+            test: /\.vue$/,
+            use: 'vue-loader',
+        }, {
+            test: /\.(png|svg|jpg)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 1024,
+                    name: '[name].[ext]'
+                }
+            }],
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin(), new CleanWebpackPlugin(), new HtmlWebpackPlugin(), new ExtractPlugin("index.css")
+    ]
+}
